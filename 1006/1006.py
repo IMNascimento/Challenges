85a0a93abb5f281d63cfee867527d9499950a180
Nascimento <trabalho.computador.sjn@gmail.com>
2023-11-03 18:10:51 -0300

    1006

---
+++ 1006/1006.py
@@ -0,0 +1,8 @@
+nota_a = float(input())
+nota_b = float(input())
+nota_c = float(input())
+peso_a = 2
+peso_b = 3
+peso_c = 5
+media = (nota_a * peso_a + nota_b * peso_b + nota_c * peso_c) / (peso_a + peso_b + peso_c)
+print("MEDIA = {:.1f}".format(media))
