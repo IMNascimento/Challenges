0fa4bb94184d0b4e8422e86473873dec8522aabb
Nascimento <trabalho.computador.sjn@gmail.com>
2023-11-03 17:02:38 -0300

    3042 correta

--- 3042/3042.py
+++ 3042/3042.py
@@ -1,19 +1,28 @@
-metros = int(input())
-pistas = []
-toques = 0
-pista_atual = 1
-for x in range(metros):
-    e, m, d = map(int, input().split())
-    pistas.append([int(e),int(m), int(d)])
+while True:
+    metros = int(input())
+    if metros == 0 or metros == None or not metros:
+        break
 
-for i in range(metros):
-    if pistas[i][pista_atual] == 1:
-        # Papai Noel precisa mudar de pista, escolhendo a que está livre
-        if pistas[i][(pista_atual + 1) % 3] == 0:
-            pista_atual = (pista_atual + 1) % 3
+    pistas = []
+    toques = 0
+    pista_atual = 1
+    for x in range(metros):
+        e, m, d = map(int, input().split())
+        pistas.append([int(e),int(m), int(d)])
+
+    for i in pistas:
+        if i[0] == 0 and i[1] == 1 and i[2] == 1:
+            calculo = 0 - pista_atual
+            pista_atual = 0
+            toques += abs(calculo)
+        elif i[0] == 1 and i[1] == 1 and i[2] == 0:
+            calculo = 2 - pista_atual
+            pista_atual = 2
+            toques += abs(calculo)
+        elif i[0] == 1 and i[1] == 0 and i[2] == 1:
+            calculo = 1 - pista_atual
+            pista_atual = 1
+            toques += abs(calculo)
         else:
-            pista_atual = (pista_atual + 2) % 3
-        toques += 1
-
-print(toques)
-#incompleto não rodou a logica
+            pass
+    print(toques)
