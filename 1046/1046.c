8965a395073ddd5248be408fe1cb67206d60f1db
Nascimento <trabalho.computador.sjn@gmail.com>
2023-11-11 18:16:55 -0300

    1046 c

---
+++ 1046/1046.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int hora_inicial, hora_final, duracao;
+
+    scanf("%d %d", &hora_inicial, &hora_final);
+
+    if (hora_inicial < hora_final) {
+        duracao = hora_final - hora_inicial;
+    } else {
+        duracao = 24 - hora_inicial + hora_final;
+    }
+
+    printf("O JOGO DUROU %d HORA(S)\n", duracao);
+
+    return 0;
+}
